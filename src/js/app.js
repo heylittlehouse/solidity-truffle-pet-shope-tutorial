8cc4b43d479c38564c5017227c837c0b931c503a
Michael Landon <michael.r.landon@gmail.com>
2018-03-05 06:55:05 -0800

    add initWeb3 function

--- src/js/app.js
+++ src/js/app.js
@@ -24,10 +24,15 @@ App = {
   },
 
   initWeb3: function() {
-    /*
-     * Replace me...
-     */
-
+    //Is there an injected web3 instance such as Mist or Chrome w/ MetaMask?
+    if (typeof web3 !== 'undefined') {
+      App.web3Provider = web3.currentProvider;
+    } else {
+      //If no injected web3 instance is detected, fall back to Ganache
+      //Fine for dev environments, insecure for production
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+    }
+    web3 = new Web3(App.web3Provider);
     return App.initContract();
   },
 
